0c8acc5b0711ed733522e9fb273a378665e05a8e
ankitkgoyal <ankitg407@gmail.com>
2025-04-05 15:40:54 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>🕉️ Welcome to Your Spiritual Dashboard</h1>
+      <p>This is your personal space for divine insights and chakra alignment.</p>
+    </div>
+  );
+}
